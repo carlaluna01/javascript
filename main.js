8c40014c805e7915abb4f0c723c4a0e1a80bd428
Carla Luna <carrlaluna01@gmail.com>
2021-06-11 19:18:01 -0300

    mejorando la lógica

--- main.js
+++ main.js
@@ -4,24 +4,20 @@ let metodoPago =  prompt('Ingrese el método de pago que va a utilizar');
 let procesoCompra
 let finalizarCompra
 
-while (procesoCompra !== "Finalizar compra") {
 
-    if (nombreCompleto !== '') {
+
+if (nombreCompleto !== '') {
     alert('Hola, ' + nombreCompleto + '. ' + 'Aquí encontrarás una lista de preparaciones dulces y saladas que mejor se adapte a tu gusto.')
-
-    }
-
-    if (saborPreparacion !== '') {
-    alert('Seleccionó el sabor ' + '' + saborPreparacion);
-    }
-
-    if (metodoPago !== '') {
-        alert('Seleccionó el método de pago ' + '' + metodoPago);
-    }
-
-    if (procesoCompra === 'Finalizar compra') {
-        alert('El detalle de su compra es: ' + 'Comprador: ' + nombreCompleto + 'El sabor elegido: ' + saborPreparacion + 'y el método de pago es: ' + metodoPago)
-    }
 }
 
+if (saborPreparacion !== '') {
+    alert('Seleccionó el sabor ' + '' + saborPreparacion);
+}
+
+if (metodoPago !== '') {
+    alert('Seleccionó el método de pago ' + '' + metodoPago);
+}
+
+
+alert('Comprador:' + ' ' + nombreCompleto + '. ' + 'El sabor elegido:' + ' ' + saborPreparacion + ' y el método de pago es:' + ' ' + metodoPago);
 
