00394b108c12368b7ed13f3d466d491ed86a09f9
Carla Luna <carrlaluna01@gmail.com>
2021-06-16 22:26:18 -0300

    agregando funcion 2

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 
 function detalleCompra() {
-let nombreCompleto = prompt('Ingrese su nombre y apellido');
-let saborPreparacion = prompt('Ingrese su sabor preferido: Salado o Dulce');
-let metodoPago =  prompt('Ingrese el método de pago que va a utilizar');
-let procesoCompra
-let finalizarCompra
+    let nombreCompleto = prompt('Ingrese su nombre y apellido');
+    let saborPreparacion = prompt('Ingrese su sabor preferido: Salado o Dulce');
+    let metodoPago =  prompt('Ingrese el método de pago que va a utilizar');
+
+    if (nombreCompleto !== '') {
+        alert('Hola, ' + nombreCompleto + '. ' + 'Aquí encontrarás una lista de preparaciones dulces y saladas que mejor se adapte a tu gusto.')
+    }
+
+    if (saborPreparacion !== '') {
+        alert('Seleccionó el sabor ' + '' + saborPreparacion);
+    }
+
+    if (metodoPago !== '') {
+        alert('Seleccionó el método de pago ' + '' + metodoPago);
+    }
 
 
-
-if (nombreCompleto !== '') {
-    alert('Hola, ' + nombreCompleto + '. ' + 'Aquí encontrarás una lista de preparaciones dulces y saladas que mejor se adapte a tu gusto.')
-}
-
-if (saborPreparacion !== '') {
-    alert('Seleccionó el sabor ' + '' + saborPreparacion);
-}
-
-if (metodoPago !== '') {
-    alert('Seleccionó el método de pago ' + '' + metodoPago);
-}
-
-
-alert('Comprador:' + ' ' + nombreCompleto + '. ' + 'El sabor elegido:' + ' ' + saborPreparacion + ' y el método de pago es:' + ' ' + metodoPago);
+    alert('Comprador:' + ' ' + nombreCompleto + '. ' + 'El sabor elegido:' + ' ' + saborPreparacion + ' y el método de pago es:' + ' ' + metodoPago);
 }
 
 
